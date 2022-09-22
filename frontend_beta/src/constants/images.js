4a0d3888ed4ac9ce0e09a3ba85c024d36ad83a0f
Simon Reinhardt <saadabou.study@gmail.com>
2022-09-22 18:58:32 +0100

    adding a design

--- frontend_beta/src/constants/images.js
+++ frontend_beta/src/constants/images.js
@@ -1,15 +1,7 @@
 import logo from '../assets/images/logo.svg'
-import p_1 from '../assets/images/patterns/p_1.jpg'
-import p_2 from '../assets/images/patterns/p_2.png'
-import p_3 from '../assets/images/patterns/p_3.png'
-import p_4 from '../assets/images/patterns/p_4.png'
 
 const images = {
   logo,
-  p_1,
-  p_2,
-  p_3,
-  p_4,
   }
 
   export default images;
