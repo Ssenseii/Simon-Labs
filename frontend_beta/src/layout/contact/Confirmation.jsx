d14ec04b93c0f764fabd6850223167cddd2e3f68
Simon Reinhardt <saadabou.study@gmail.com>
2022-09-26 11:56:16 +0100

    cleaning up the css

--- frontend_beta/src/layout/contact/Confirmation.jsx
+++ frontend_beta/src/layout/contact/Confirmation.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Confirmation = (props) => {
   return (
-    <div className={`confirmation ${props.visibility}`}>
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      className={`confirmation ${props.visibility}`}
+    >
       <h1 className={props.color}>Message {props.confirm}</h1>
-    </div>
+    </motion.div>
   );
 };
 
--- frontend_beta/src/layout/home/Hero.jsx
+++ frontend_beta/src/layout/home/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
         <div className="hero__actions">
           <a href="#focus">
             <button>
-              Take a Look
+              My Projects
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
